4ec480dc9b0eb91e0a93b424e137b04202d10708
AllanRedflame <114324239+AllanRedflame@users.noreply.github.com>
2023-09-16 16:35:09 -0600

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Reflection;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        static void displayWelcome() {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        displayWelcome();
+
+        static string promptUserName() {
+            Console.WriteLine("What's your name?");
+            string nameQuery = Console.ReadLine();
+            return nameQuery;
+        }
+
+
+
+        static int promptUserNumber() {
+            Console.WriteLine("What's your favorite number?");
+            string numberQuery = Console.ReadLine();
+            return int.Parse(numberQuery);
+        }
+
+        static int squareNumber(int inputNumber) {
+            int squaredNumber = inputNumber * inputNumber;
+            return squaredNumber;
+        }
+
+        static void displayResult(string name, int square) {
+            Console.WriteLine($"Your name is {name} and your squared number is {square}");
+
+        }
+        
+        displayResult(promptUserName(), squareNumber(promptUserNumber()));
     }
+
 }
