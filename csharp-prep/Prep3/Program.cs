7c0e00aef8ef8d5cbfa785e53665415c29d0680b
AllanRedflame <114324239+AllanRedflame@users.noreply.github.com>
2023-09-16 16:31:34 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumberInt = randomGenerator.Next(1, 10);
+
+
+        int guessInt = 0;
+        int guessCounter = 0;
+        string continuing = "yes";
+
+        while (continuing == "yes") {
+            Console.WriteLine("Make a guess! (Between 1 and 10)");
+            string guess = Console.ReadLine();
+            guessInt = int.Parse(guess);
+
+            if (guessInt > magicNumberInt) {
+                Console.WriteLine("That's too high!");
+                guessCounter ++;
+            }
+
+            else if (guessInt < magicNumberInt) {
+                Console.WriteLine("That's too low!");
+                guessCounter ++;
+            }
+
+            else {
+                Console.WriteLine($"Good job, you got it! You made {guessCounter} guesses.");
+                Console.WriteLine("Would you like to play again? (Enter yes or no)");
+                continuing = Console.ReadLine();
+                guessCounter = 0;
+                
+            }
+    }
+
     }
 }
